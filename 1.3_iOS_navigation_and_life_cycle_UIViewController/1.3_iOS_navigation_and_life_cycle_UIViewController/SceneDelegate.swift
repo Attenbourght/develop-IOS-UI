bfdd2a395dac25896fc474c7f399a0cdfee4b427
Attenbourght <spevarnikita4@gmail.com>
2022-05-27 06:35:47 +0300

    finished homework №2.2

--- 1.3_iOS_navigation_and_life_cycle_UIViewController/1.3_iOS_navigation_and_life_cycle_UIViewController/SceneDelegate.swift
+++ 1.3_iOS_navigation_and_life_cycle_UIViewController/1.3_iOS_navigation_and_life_cycle_UIViewController/SceneDelegate.swift
@@ -9,7 +9,7 @@ import UIKit
 
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     
-    var window: UIWindow? //Создаем окно, в котором будем работать
+    var window: UIWindow? 
     
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         guard let windowScene = (scene as? UIWindowScene) else { return }
--- 1.3_iOS_navigation_and_life_cycle_UIViewController/1.3_iOS_navigation_and_life_cycle_UIViewController/ViewController.swift
+++ 1.3_iOS_navigation_and_life_cycle_UIViewController/1.3_iOS_navigation_and_life_cycle_UIViewController/ViewController.swift
@@ -19,23 +19,56 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .systemBackground
-        makeButton()
+        setupLayout()
     }
     
-    private func makeButton() {
-        let button = UIButton(frame: CGRect(x: 0, y: 0, width: 200, height: 40))
-        button.center = view.center
-        button.setTitle("Пост", for: .normal)
-        button.backgroundColor = .systemGray
-        button.layer.cornerRadius = 4
-        button.addTarget(self, action: #selector(buttonAction), for: .touchUpInside)
-        view.addSubview(button)
+    private let stackView: UIStackView = {
+        let stackView = UIStackView()
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        stackView.axis = .vertical
+        stackView.spacing = 10
+        return stackView
+    }()
+    
+    private lazy var firstButton: UIButton = {
+        let firstButton = UIButton()
+        firstButton.translatesAutoresizingMaskIntoConstraints = false
+        firstButton.setTitle("Тут все таки могла быть реклама!", for: .normal)
+        firstButton.backgroundColor = .systemGray
+        firstButton.layer.cornerRadius = 12
+        firstButton.addTarget(self, action: #selector(buttonAction), for: .touchUpInside)
+        return firstButton
+    }()
+    
+    @objc func buttonAction(sender: UIButton!) {
+        self.navigationController?.pushViewController(NewViewController(), animated: true)
     }
 
-    @objc private func buttonAction() {
-        let postView = NewViewController()
-        postView.postTitle = post.title
-        navigationController?.pushViewController(postView, animated: true)
+    
+    private lazy var secondButton: UIButton = {
+        let secondButton = UIButton()
+        secondButton.translatesAutoresizingMaskIntoConstraints = false
+        secondButton.setTitle("Вторая кнопка", for: .normal)
+        secondButton.backgroundColor = .systemGray
+        secondButton.layer.cornerRadius = 12
+        secondButton.addTarget(self, action: #selector(buttonAction), for: .touchUpInside)
+        return secondButton
+    }()
+    
+    private func setupLayout() {
+        view.addSubview(stackView)
+        [firstButton, secondButton].forEach { stackView.addArrangedSubview($0) }
+        
+        NSLayoutConstraint.activate([
+            stackView.centerYAnchor.constraint(equalTo: view.safeAreaLayoutGuide.centerYAnchor),
+            stackView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 16),
+            stackView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -16),
+            stackView.heightAnchor.constraint(equalToConstant: 110),
+           
+            firstButton.heightAnchor.constraint(equalToConstant: 50),
+           
+            secondButton.heightAnchor.constraint(equalToConstant: 50)
+        ])
     }
 }
 
